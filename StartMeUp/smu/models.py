7c15707cba75a76446776faee06f73c4059192eb
NandoSerrano85 <fserr009@fiu.edu>
2018-02-03 02:54:37 -0500

    updating model still need map and legislation

--- StartMeUp/smu/models.py
+++ StartMeUp/smu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
+class User(model.Model):
+    EIN = models.IntegerFields(primary_key=True, max_length=9)
+    email = models.EmailField()
+    business_name = models.CharField(max_length=300)
+
 # Create your models here.
